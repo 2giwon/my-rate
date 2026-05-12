70bb542dbd006a46febd1cc508693097b78091a8
Giwon Lee <2giwon@gmail.com>
2026-05-12 18:17:12 +0900

    feat(data): Riverpod providers (prefs, dio, api, repo, settings)

---
+++ lib/data/exchange_rate/providers.dart
@@ -0,0 +1,60 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../domain/exchange_rate/exchange_rate_repository.dart';
+import 'currency_catalog.dart';
+import 'exchange_rate_repository_impl.dart';
+import 'local/favorites_store.dart';
+import 'local/rate_cache.dart';
+import 'local/settings_store.dart';
+import 'remote/exchange_rate_api.dart';
+
+part 'providers.g.dart';
+
+@Riverpod(keepAlive: true)
+Future<SharedPreferences> sharedPreferences(SharedPreferencesRef ref) =>
+    SharedPreferences.getInstance();
+
+@Riverpod(keepAlive: true)
+Future<CurrencyCatalog> currencyCatalog(CurrencyCatalogRef ref) async {
+  final c = CurrencyCatalog();
+  await c.load();
+  return c;
+}
+
+@Riverpod(keepAlive: true)
+Dio dio(DioRef ref) {
+  return Dio(BaseOptions(
+    connectTimeout: const Duration(seconds: 8),
+    receiveTimeout: const Duration(seconds: 8),
+  ));
+}
+
+const String _kApiKeyDefine = 'EXCHANGE_RATE_API_KEY';
+const String _kApiKey = String.fromEnvironment(_kApiKeyDefine);
+
+@Riverpod(keepAlive: true)
+ExchangeRateApi exchangeRateApi(ExchangeRateApiRef ref) {
+  return ExchangeRateApi(dio: ref.watch(dioProvider), apiKey: _kApiKey);
+}
+
+@Riverpod(keepAlive: true)
+Future<ExchangeRateRepository> exchangeRateRepository(
+    ExchangeRateRepositoryRef ref) async {
+  final prefs = await ref.watch(sharedPreferencesProvider.future);
+  final catalog = await ref.watch(currencyCatalogProvider.future);
+  return ExchangeRateRepositoryImpl(
+    api: ref.watch(exchangeRateApiProvider),
+    cache: RateCache(prefs),
+    favorites: FavoritesStore(prefs),
+    catalog: catalog,
+  );
+}
+
+@Riverpod(keepAlive: true)
+Future<SettingsStore> settingsStore(SettingsStoreRef ref) async {
+  final prefs = await ref.watch(sharedPreferencesProvider.future);
+  return SettingsStore(prefs);
+}
---
+++ lib/data/exchange_rate/providers.g.dart
@@ -0,0 +1,114 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sharedPreferencesHash() => r'25eceea0052302f519f44a896409ba30ede45562';
+
+/// See also [sharedPreferences].
+@ProviderFor(sharedPreferences)
+final sharedPreferencesProvider = FutureProvider<SharedPreferences>.internal(
+  sharedPreferences,
+  name: r'sharedPreferencesProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sharedPreferencesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SharedPreferencesRef = FutureProviderRef<SharedPreferences>;
+String _$currencyCatalogHash() => r'361c227bf9ffefdeaa6e2f21965b2771762a2a9b';
+
+/// See also [currencyCatalog].
+@ProviderFor(currencyCatalog)
+final currencyCatalogProvider = FutureProvider<CurrencyCatalog>.internal(
+  currencyCatalog,
+  name: r'currencyCatalogProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$currencyCatalogHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CurrencyCatalogRef = FutureProviderRef<CurrencyCatalog>;
+String _$dioHash() => r'1a612b72c173f426641bc9180e514895bed67a36';
+
+/// See also [dio].
+@ProviderFor(dio)
+final dioProvider = Provider<Dio>.internal(
+  dio,
+  name: r'dioProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dioHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DioRef = ProviderRef<Dio>;
+String _$exchangeRateApiHash() => r'362a32a3c691392cc23ecfd7fce5e4ec3b915b97';
+
+/// See also [exchangeRateApi].
+@ProviderFor(exchangeRateApi)
+final exchangeRateApiProvider = Provider<ExchangeRateApi>.internal(
+  exchangeRateApi,
+  name: r'exchangeRateApiProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$exchangeRateApiHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ExchangeRateApiRef = ProviderRef<ExchangeRateApi>;
+String _$exchangeRateRepositoryHash() =>
+    r'696bb0971a34e4677d3c21ec6113ac3ca89e6046';
+
+/// See also [exchangeRateRepository].
+@ProviderFor(exchangeRateRepository)
+final exchangeRateRepositoryProvider =
+    FutureProvider<ExchangeRateRepository>.internal(
+      exchangeRateRepository,
+      name: r'exchangeRateRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$exchangeRateRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ExchangeRateRepositoryRef = FutureProviderRef<ExchangeRateRepository>;
+String _$settingsStoreHash() => r'1af776055e5f4f81400796fd576b6bef20bc52a2';
+
+/// See also [settingsStore].
+@ProviderFor(settingsStore)
+final settingsStoreProvider = FutureProvider<SettingsStore>.internal(
+  settingsStore,
+  name: r'settingsStoreProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$settingsStoreHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SettingsStoreRef = FutureProviderRef<SettingsStore>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
