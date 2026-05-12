ce055cadf13336321fbaa85b7b1a1062ecba3b3e
Giwon Lee <2giwon@gmail.com>
2026-05-12 17:55:09 +0900

    style: dart format core/

--- lib/core/constants/popular_currencies.dart
+++ lib/core/constants/popular_currencies.dart
@@ -1,6 +1,15 @@
 /// 한국 사용자가 자주 쓰는 인기 통화 (Currency Picker '인기' 섹션 노출 순서)
 const List<String> kPopularCurrencyCodes = [
-  'USD', 'EUR', 'JPY', 'CNY', 'GBP',
-  'AUD', 'CAD', 'HKD', 'SGD',
-  'THB', 'VND', 'PHP',
+  'USD',
+  'EUR',
+  'JPY',
+  'CNY',
+  'GBP',
+  'AUD',
+  'CAD',
+  'HKD',
+  'SGD',
+  'THB',
+  'VND',
+  'PHP',
 ];
